9554bfbf11e59187908108ec3bd2ba2171cb44e0
unknown <nachiket919156@gmail.com>
2024-04-05 01:04:30 +0530

    first

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -26,7 +26,48 @@ public class ReservationServiceImpl implements ReservationService {
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
 
         // Retrieve user
-        return reservationRepository3.save(reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels));
+        try {
+            Reservation reservation = new Reservation();
+            reservation.setNumberOfHours(timeInHours);
+            User user = userRepository3.findById(userId).get();
+            ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+            Spot spot1 = getSpot(numberOfWheels, parkingLot);
+            reservation.setSpot(spot1);
+            reservation.setUser(user);
+            user.getReservationList().add(reservation);
+            spot1.getReservationList().add(reservation);
+            spotRepository3.save(spot1);
+            //reservationRepository3.save(reservation);
+            userRepository3.save(user);
+            return reservation;
+        }
+        catch(Exception e){
+            return null;
+        }
 
+    }
+    private static Spot getSpot(Integer numberOfWheels, ParkingLot parkingLot) throws Exception {
+        List<Spot> spotList = parkingLot.getSpotList();
+        Spot spot1 = null;
+        int price = Integer.MAX_VALUE;
+        for (Spot spot : spotList) {
+            int wheel = 0;
+            if (spot.getSpotType() == SpotType.TWO_WHEELER) {
+                wheel = 2;
+            } else if (spot.getSpotType() == SpotType.FOUR_WHEELER) {
+                wheel = 4;
+            } else if (spot.getSpotType() == SpotType.OTHERS) {
+                wheel = 99;
+            }
+            if (spot.getOccupied() == false && wheel > numberOfWheels && spot.getPricePerHour() < price) {
+                spot1 = spot;
+                price = spot.getPricePerHour();
+            }
+        }
+        if (spot1 == null) {
+            throw new Exception();
+        }
+        spot1.setOccupied(true);
+        return spot1;
     }
 }
